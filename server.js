2e1242e9e76dc6eb2e37007b7cf95223f0922ae5
Martin Bing <bing.martin.photography@gmail.com>
2015-01-04 17:26:12 +0100

    Add index.html file

--- server.js
+++ server.js
@@ -6,10 +6,29 @@ var fs = require('fs');
 var root = __dirname;
 
 var server = http.createServer(function (req, res) {
-  var url = parse(req.url);
-  var path = join(root, url.pathname);
-  var stream = fs.createReadStream(path);
-  stream.pipe(res);
+    var url = parse(req.url);
+    var path = join(root, url.pathname);
+    fs.stat(path, function (err, stat) {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                res.statusCode = 404;
+                res.end('File Not Found');
+            }
+            else {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            }
+        }
+        else {
+            var stream = fs.createReadStream(path);
+            res.setHeader('Content-Length', stat.size);
+            stream.pipe(res);
+            stream.on('error', function (err) {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            });
+        }
+    });
 });
 server.listen(9000, function(){
    console.log('listening on 9000');
