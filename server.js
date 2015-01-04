2034c48010588cb6215ca742c65e18e14ec669df
Martin Bing <bing.martin.photography@gmail.com>
2015-01-04 17:16:43 +0100

    Add server and package file

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var parse = require('url').parse;
+var join = require('path').join;
+var fs = require('fs');
+
+var root = __dirname;
+
+var server = http.createServer(function (req, res) {
+  var url = parse(req.url);
+  var path = join(root, url.pathname);
+  var stream = fs.createReadStream(path);
+  stream.pipe(res);
+});
+server.listen(9000, function(){
+   console.log('listening on 9000');
+});
