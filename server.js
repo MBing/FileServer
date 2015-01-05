78311280ed629cc052ebc0c99a2a8e078ea8ea53
Martin Bing <bing.martin.photography@gmail.com>
2015-01-05 08:38:13 +0100

    Add server functionality root dir

--- server.js
+++ server.js
@@ -2,12 +2,33 @@ var http = require('http');
 var parse = require('url').parse;
 var join = require('path').join;
 var fs = require('fs');
+var qs = require('querystring');
 
 var root = __dirname;
 
 var server = http.createServer(function (req, res) {
+	if(req.url == '/') {
+	    switch(req.method) {
+	        case 'GET':
+	            req.url = '/index.html';
+	        break;
+	        case 'POST':
+	            var item = '';
+	            req.setEncoding('utf8');
+	            req.on('data', function(chunk){
+	                item += chunk;
+	            });
+	            req.on('end', function(){
+	                var obj = qs.parse(item);
+	                res.end('The item: "' + obj.item + '" was added successfully');
+	            });
+	        break;
+	    }
+	}
+	
     var url = parse(req.url);
     var path = join(root, url.pathname);
+
     fs.stat(path, function (err, stat) {
         if (err) {
             if (err.code == 'ENOENT') {
